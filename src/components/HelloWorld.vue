91d3ace17e7659fd3247d912d85c101e18f981be
elderdog <yuanxiang0201@163.com>
2023-05-18 17:00:02 +0800

    feat: run process

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Waiter, { BurgerType } from '../classes/waiters/Waiter'
+import BurgerDisplay from '../classes/displays/BurgerDisplay'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const logs = ref<string[]>([])
+
+function start() {
+  const burgerDisplay = new BurgerDisplay()
+  burgerDisplay.onDuty()
+  const waiter = new Waiter()
+  waiter.onDuty()
+  waiter.takeOrder(BurgerType.BEEF, true, ['cheese'])
+  waiter.on('meal-ready', meal => logs.value.push(meal.getName()))
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <button type="button" @click="start">Start</button>
+  <ul class="log">
+    <li v-for="log in logs" :key="log"><code>{{ log }}</code></li>
+  </ul>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.log {
+  text-align: left;
 }
 </style>
