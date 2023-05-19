bcb49fd2630f9350949847273a5cf211bce3a50c
elderdog <yuanxiang0201@163.com>
2023-05-19 11:49:12 +0800

    fix: off meal-ready listener in callback

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Waiter, { BurgerType } from '../classes/waiters/Waiter'
 import BurgerDisplay from '../classes/displays/BurgerDisplay'
+import type { IMeal } from '../classes/meals/Meal'
 import { useLoggerStore } from '../stores/logger'
 
 defineProps<{ msg: string }>()
@@ -8,16 +9,25 @@ defineProps<{ msg: string }>()
 const logger = useLoggerStore()
 
 function start() {
+  /**
+   * BurgerDisplay and Waiter are completely decoupled,
+   * and their communication during the process execution is solely maintained by the EventBus,
+   * making it difficult to see the relationship between the two,
+   * However, BurgerDisplay is still a necessary condition for the process execution,
+   * Is this too loose of a coupling between the two? Do they need a direct connection?
+   */
   const burgerDisplay = new BurgerDisplay()
   burgerDisplay.onDuty()
   const waiter = new Waiter()
   waiter.onDuty()
   waiter.takeOrder(BurgerType.BEEF, true, ['cheese', 'tomato'])
-  waiter.on('meal-ready', meal => {
+  const readyCallback = (meal: IMeal) => {
     logger.info(`[Client]: get ready to eat ${meal.getName()}`)
+    waiter.off('meal-ready', readyCallback)
     burgerDisplay.offDuty()
     waiter.offDuty()
-  })
+  }
+  waiter.on('meal-ready', readyCallback)
 }
 </script>
 
